41d64f0c283ef845d67e314b1cdc163beeb32648
t_ziaei <t_ziaei@isc.co.ir>
2022-11-22 21:02:16 +0330

    Halat behineye tamrine 2.

--- S05-loopExercise.py
+++ S05-loopExercise.py
@@ -3,13 +3,13 @@
 # برنامه اي بنويسيد كه دو عدد را از كاربر گرفته و اعداد مابين آن ها را نمايش دهد
 # ln1 = list(map(int, input('Enter 2 number: ').split(',')))
 # print(ln1)
-# for i in range(ln1[0]+1, ln1[1]):
+# for i in range(min(ln1)+1, max(ln1)):
 #     print(i, end=' ')
 
 # ------------------2--------------------
 # برنامه اي بنويسيد كه دو عدد صحيح را گرفته و مقسوم عليه هاي مشتركشان را نمايش دهد
-# ls1, ls2 = [], []
-# ln1 = list(map(int, input('Enter 2 number: ').split(',')))
+ls1, ls2 = [], []
+ln1 = list(map(int, input('Enter 2 number: ').split(',')))
 # for i in range(1, ln1[0] + 1):
 #     if ln1[0] % i == 0:
 #         ls1.append(i)
@@ -19,6 +19,10 @@
 # print(ln1[0], ls1, '\n', ln1[1], ls2, sep='')
 # s1 = set(ls1).intersection(set(ls2))
 # print(s1)
+#------------ Halate behineh --------------
+for i in range(1,min(ln1)+1):
+    if ln1[0] % i == 0 and ln1[1] % i == 0:
+        print(i,end=' ')
 
 # ------------------3--------------------
 # برنامه اي بنويسيد كه دو عدد صحيح را گرفته و بزرگترين مقسوم عليه مشتركشان را نمايش دهد
@@ -48,24 +52,24 @@
 #     print('*'*i)
 
 # ------------------6--------------------
-from random import choice
-
-familyTuple=('Ali', 'Taher', 'Mozhgan', 'Noyan', 'Mojtaba'
-          ,'Morteza','Mostafa','Tahmouress','Elahe','Avina')
-systemGuessTuple=set()
-print(familyTuple)
-i=1
-while i<=len(familyTuple):
-    name=choice(familyTuple)
-    if name in systemGuessTuple:
-        continue
-    systemGuessTuple.add(name)
-    print(f'{i}: {name}')
-    ans=input('Is it true? (y/n) ')
-    if ans.lower()=='y':
-        print('*'*5,'FINISH','*'*5)
-        print(f'{name} is the name.')
-        exit(0)
-    i+=1
-print('Are you kidding me!')
-exit(13)
+# from random import choice
+#
+# familyTuple=('Ali', 'Taher', 'Mozhgan', 'Noyan', 'Mojtaba'
+#           ,'Morteza','Mostafa','Tahmouress','Elahe','Avina')
+# systemGuessTuple=set()
+# print(familyTuple)
+# i=1
+# while i<=len(familyTuple):
+#     name=choice(familyTuple)
+#     if name in systemGuessTuple:
+#         continue
+#     systemGuessTuple.add(name)
+#     print(f'{i}: {name}')
+#     ans=input('Is it true? (y/n) ')
+#     if ans.lower()=='y':
+#         print('*'*5,'FINISH','*'*5)
+#         print(f'{name} is the name.')
+#         exit(0)
+#     i+=1
+# print('Are you kidding me!')
+# exit(13)
