384c6d4666fd5ce657879cfeaa282ad9aab0db18
taher <ziaee.taher@gmail.com>
2022-11-25 21:13:39 +0330

    syntaxe neveshtane arguman-ha finished.

--- Session06.py
+++ Session06.py
@@ -40,19 +40,40 @@
 # Problem? 1! + 2! + 3! + ... n!
 # Bar asase osoole Good Coding har tabe' bayad vazifeye khasi ra anjam dahad.
 # Leza ma yek tabe' minevisim ke Factorial ra hesab konad & yek tabe' ke Factorial-ha ra ba ham + konad.
-def fact(n):
-    f = 1
-    for i in range(1, n + 1):
-        f *= i
-    return f
+# def fact(n):
+#     f = 1
+#     for i in range(1, n + 1):
+#         f *= i
+#     return f
+#
+#
+# def sum_fact(n):
+#     s = 0
+#     for i in range(1, n + 1):
+#         s += fact(i)
+#     return s
+#
+#
+# n = int(input('Enter fact number: '))
+# print(f'Sum factorial {n}! = {sum_fact(n)}')
+# --------------------------------
+
+def func(a, b, c):
+    print('a =', a)
+    print('b =', b)
+    print('c =', c)
 
 
-def sum_fact(n):
-    s = 0
-    for i in range(1, n + 1):
-        s += fact(i)
-    return s
-
-
-n = int(input('Enter fact number: '))
-print(f'Sum factorial {n}! = {sum_fact(n)}')
+# normal
+func(2, 3, 5)
+# name = value
+func(b=4, c=11, a=-3)
+# normal + name = value
+func(2, b=2, c=6)
+# itrable - (str,list,tuple,set)
+func(*[4, 8, 15])
+x = [-3, 7, 17]
+func(*x)
+# **dict
+d = {'a': 4, 'b': 12, 'c': 1}
+func(**d)
