447d3abcbe26283be5a53bd6171f0a1718b9fa32
Taher Ziyaei <ziaee.taher@gmail.com>
2023-01-14 04:37:04 +0330

    S06-recursive-function-Exr.py is finished.

--- S06-TicTacToe.py
+++ S06-TicTacToe.py
@@ -1,5 +1,9 @@
+from colorama import Fore, Back
+
 board = list(range(1, 10))
+# board = [1, 2, "X", 4, 5, 6, "O", 8, 9]
 player, computer = "X", "O"
+winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 
 # Player: X
 # Computer: O
@@ -8,11 +12,18 @@ print(f"Player: {player}\nComputer: {computer}\n")
 
 # safheye bazi ra chap mikonad. list mazkoor ra be soorate matrixe 3*3 chap mikonad
 def print_board():
+    j = 1
     for i in board:
         spliter = " " * 4
-        if i % 3 == 0:
+        if j % 3 == 0:
             spliter = "\n\n"
-        print(f"[{i}]", end=spliter)
+        if i == "X":
+            print(Fore.BLUE + f"[{i}]" + Fore.RESET, end=spliter)
+        elif i == "O":
+            print(Fore.RED + f"[{i}]" + Fore.RESET, end=spliter)
+        else:
+            print(f"[{i}]", end=spliter)
+        j += 1
 
 
 # Check mikonad ke tamame khaneha por shode ast ya na. dar vaghe check mikonad ke bazi tamam shode ast ya na
@@ -20,6 +31,47 @@ def print_board():
 def has_empty_space():
     return board.count("X") + board.count("O") != 9
 
-print_board()
-# while has_empty_space():
-#     print_board()
+def can_move(brd, mve):
+    # in shart barrasi mikonad ke agar meghdare vared shode beyne 1-9 bashad & adad bashad(yani ghabele harkat bashad)
+    if mve in range(1, 10) and isinstance(brd[mve - 1], int):
+        return True
+    return False
+
+def make_move(brd, plyr, mve, undo=False):
+    if can_move(brd, mve):
+        brd[mve - 1] = plyr
+        win = is_winner(brd, plyr)
+        if undo:
+            brd[mve - 1] = mve
+        return True, win
+    return False,False
+
+
+def is_winner(brd, plyr):
+    win = True
+    for tpl in winners:
+        win = True
+        for j in tpl:
+            if brd[j] != plyr:
+                win = False
+                break
+        if win:
+            break
+    return win
+
+
+
+
+
+# print_board()
+while has_empty_space():
+    print_board()
+    move = int(input("Choose your move(1-9): "))
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print("Invalid number! Please try again...")
+        continue
+    if won:
+        print(Fore.GREEN+"Congratulation, You won!"+Fore.RESET)
+        print_board()
+        break
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-ls = ['taher', 'mozhgan', 'noyan', 'tahmoures']
-i = 0
-while i < len(ls):
-    s = ls[i]
-    j = 0
-    while j < len(s):
-        if not (j % 2 == 0):
-            print(s[j].upper(), end='')
-        else:
-            print(s[j], end='')
-        j += 1
-    print()
-    i += 1
+# ls = ['taher', 'mozhgan', 'noyan', 'tahmoures']
+# i = 0
+# while i < len(ls):
+#     s = ls[i]
+#     j = 0
+#     while j < len(s):
+#         if not (j % 2 == 0):
+#             print(s[j].upper(), end='')
+#         else:
+#             print(s[j], end='')
+#         j += 1
+#     print()
+#     i += 1
+
+#------------------------
+
+from termcolor import colored
+from colorama import Fore,Back
+print(colored("Hi baby","red"))
+print(Fore.RED+"ISC")
