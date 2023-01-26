4091c3a5ed8a1f0a6bd9d1b69514d4f8b5485eb7
Taher Ziyaei <ziaee.taher@gmail.com>
2023-01-26 07:33:15 +0330

    S07-comprehension-Part1.py is finished.

---
+++ S07-comprehension-Part1.py
@@ -0,0 +1,26 @@
+# print("------- normal --------")
+# s = []
+# for i in range(10):
+#     s.append(i ** 2)
+# print(s)
+#
+# print("------- comprehension -------")
+# x = [j ** 2 for j in range(10)]
+# print(x)
+
+# ====================================
+
+# from math import pi
+#
+# z = [round(pi, i) for i in range(10)]
+# print(z)
+
+# ====================================
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11]
+]
+
+print(matrix[1][2])
