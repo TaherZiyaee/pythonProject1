10a274684252a6bc8b49675e10ef926b84e0c78b
Taher Ziyaei <ziaee.taher@gmail.com>
2023-01-27 23:00:05 +0330

    S07-comprehension-Part2.py is finished.

---
+++ S07-comprehension-Part2.py
@@ -0,0 +1,15 @@
+# print("------- normal --------")
+# x=[i for i in (1,2,3)]
+# print(x)
+#
+# print("------- generator express --------")
+# x=(i for i in (1,2,3))
+# print(list(x))
+
+# from random import randrange
+# def func():
+#     return randrange(50,150)
+# x=[n for _ in range(10) if (n:=func()) > 100]
+# print(x)
+
+print(chr(6528))
