719036570db930fd2e5284171a07b9e065c07142
Taher Ziyaei <ziaee.taher@gmail.com>
2023-02-19 07:46:10 +0330

    S08 is in-progress.

--- mytest1.py
+++ mytest1.py
@@ -1,2 +1,5 @@
-def func1(a):
-    print(a**2)
+# from pkg2.mod1 import *
+# func1(2,3)
+
+from pkg1 import mod2
+print(help(mod2))
---
+++ pkg2/mod1.py
@@ -0,0 +1,10 @@
+__all__ = ["func1", "b"]
+
+def func1(x, y):
+    print(pow(x, y))
+
+def func2(name):
+    print(name)
+
+a = 17
+b = "Jafar"
