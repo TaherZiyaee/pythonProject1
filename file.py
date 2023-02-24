c712f7e7df01d3f12c71bc309b87a02f89f32297
Taher Ziyaei <ziaee.taher@gmail.com>
2023-02-24 23:45:09 +0330

    S09 is started.

---
+++ file.py
@@ -0,0 +1,15 @@
+open(file="note.txt", mode="r")
+open("note.txt", "r")
+
+import os
+print(os.getcwd())
+
+f=open("note10.txt","w")
+print(f.write("reza"))
+
+k=open("note.txt","r")
+print(k.readline())
+print(k.readline())
+
+k=open("note.txt","r")
+print(k.readlines())
