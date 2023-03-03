bfdc0d4fc55cf0bda508a8e43c9471153d4783e0
Taher Ziyaei <ziaee.taher@gmail.com>
2023-03-03 07:49:24 +0330

    S09 is started.

---
+++ file2.py
@@ -0,0 +1,16 @@
+# f = open("note10.txt", "w")
+# f.write("Taher Ziaei\n")
+# f.write("Noyan Ziaei\n")
+# f.flush()
+# input("Continue...")
+# f.close()
+
+# f = open("note10.txt", "wb", buffering=0)
+# f.write(b"Bruno Fernandez\n")
+# f.write(b"David Degea\n")
+# f.write(b"Marcus Rashford\n")
+# input("Continue...")
+# f.close()
+
+import io
+print(io.DEFAULT_BUFFER_SIZE)
