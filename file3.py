39eb0bcc83ef7b59caf0abbcc72bd92fd4ce9397
TAHER_Home <ziaee.taher@gmail.com>
2023-03-12 13:31:48 +0330

    home s09

--- file3.py
+++ file3.py
@@ -1,6 +1,14 @@
-f = open("note10.txt", "rb")
-print(f.tell())
-print(f.read(17))
-f.seek(-3, 2)
-print(f.tell())
-print(f.read(5))
+# f = open("note10.txt", "rb")
+# print(f.tell())
+# print(f.read(17))
+# f.seek(-3, 2)
+# print(f.tell())
+# print(f.read(5))
+
+# import os
+# if os.path.exists("note2.txt"):
+#     f=open("note2.txt","r")
+#     print(f.readlines())
+
+import os
+os.remove("note2.txt")
